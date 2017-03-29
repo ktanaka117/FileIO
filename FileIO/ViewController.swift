2ead07a4aca4d8f8a93abc2939fd8872e28f155f
田中賢治 <key.m4096.kung.fu@gmail.com>
2017-03-29 16:16:52 +0900

    ディレクトリ作成、ファイルの読み込みを追加

--- FileIO/ViewController.swift
+++ FileIO/ViewController.swift
@@ -13,9 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        makeDirectory()
         downloadImage()
     }
 
+    func makeDirectory() {
+        if let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last {
+            let url = URL(string: documentDirectoryURL.absoluteString + "/hoge")!
+            let fm = FileManager.default
+            do {
+                try fm.createDirectory(at: url, withIntermediateDirectories: false, attributes: nil)
+            } catch {
+                print("failed to create directory.")
+            }
+        }
+    }
+    
     func downloadImage() {
         let imageURL = URL(string: "https://pbs.twimg.com/profile_images/836130575224201216/ykkPUBOE.jpg")!
         let task = URLSession.shared.dataTask(with: imageURL) { [weak self] data, response, error in
@@ -30,7 +43,7 @@ class ViewController: UIViewController {
         let imageFileName = "ktanaka117.jpg"
         
         if let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last {
-            let targetImageFilePath = documentDirectoryURL.appendingPathComponent(imageFileName)
+            let targetImageFilePath = documentDirectoryURL.appendingPathComponent("hoge/\(imageFileName)")
             
             print("targetImageFilePath: \(targetImageFilePath)")
             
@@ -39,7 +52,18 @@ class ViewController: UIViewController {
             } catch let error {
                 print(error)
             }
+            
+            readFile()
         }
+    }
+    
+    func readFile() {
+        guard let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last else { return }
+        
+        let targetImageFilePath = documentDirectoryURL.appendingPathComponent("hoge/ktanaka117.jpg")
+        let data = try! Data(contentsOf: targetImageFilePath)
+        
+        print(data)
     }
     
 }
